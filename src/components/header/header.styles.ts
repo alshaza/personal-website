dbb1ad07a2ec35e2c9ce93be0367103488332e0e
Rami <rami.alshaza@gmail.com>
2026-03-16 01:21:48 +0100

    Coming Soon page with logo

---
+++ src/components/header/header.styles.ts
@@ -0,0 +1,15 @@
+import { Stack, styled } from "@mui/material";
+
+export const HeaderContainer = styled(Stack)({
+  width: "100%",
+  justifyContent:"center",
+  flexDirection:"column",
+  alignItems:"center",
+  backgroundColor:"transparent"
+});
+
+export const HeaderLogo = styled("img")({
+  width: "50%",
+  height: "50%",
+});
+  
